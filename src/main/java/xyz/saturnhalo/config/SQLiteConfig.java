8d0afc0c1fef3ac9c67619dea54d57e07b63c2a0
AsYukiZzz <970285047@qq.com>
2026-03-11 22:29:17 +0800

    fix: 修复了 SQLiteConfig 里因为导包错误导致的倒错

--- src/main/java/xyz/saturnhalo/config/SQLiteConfig.java
+++ src/main/java/xyz/saturnhalo/config/SQLiteConfig.java
@@ -1,14 +1,14 @@
 package xyz.saturnhalo.config;
 
 import com.zaxxer.hikari.HikariDataSource;
-import org.springframework.boot.jdbc.autoconfigure.DataSourceProperties;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import javax.sql.DataSource;
 
 /**
- * SQLite 配置类
+ * SQLite 配置
  */
 @Configuration
 public class SQLiteConfig {
